85c5ede8b09a0382f92deaf93836dd48737a558b
Brendo Oliveira <brendo.dev@outlook.com>
2022-12-08 11:05:06 -0300

    update name

--- themes/app/_theme.php
+++ themes/app/_theme.php
@@ -39,7 +39,7 @@
 <div class="ajax_response"><?= flash(); ?></div>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container">
-    <a class="navbar-brand logo" href="<?= url("/") ?>">Coffe Code</a>
+    <a class="navbar-brand logo" href="<?= url("/") ?>">brdev</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
--- themes/welcome/home.php
+++ themes/welcome/home.php
@@ -5,8 +5,8 @@
 
 <div class="container mt-5 text-center ">
     <i class="bi bi-cup-hot-fill" style="font-size: 2em; color:blue"></i>
-    <h2 class="logo mb-3 text-white">COFFE CODE</h2>
-    <p class="text-white">version Coffe Code 2.0 - PHP version: 8.0</p>
+    <h2 class="logo mb-3 text-white">BRDEV FRAMEWORK</h2>
+    <p class="text-white">version brdev framework 2.0 - PHP version: 8.0</p>
 
 </div>
 
